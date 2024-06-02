a596f2d941abecc462c76ae40f4d8f87b431a4c2
Oleksandr Ostrenko <oleksandr@ostrenko.dev>
2024-06-02 11:51:47 +0200

    Do not build on windows & build for other versions

--- ci/windows-latest.ps1
+++ ci/windows-latest.ps1
@@ -1,3 +1,14 @@
 
-vcpkg install gsl gsl:x64-windows boost-headers
 
+# Write-Host "VCPKG_ROOT=$env:VCPKG_ROOT"
+# Write-Host "CMAKE_ARGS=$env:CMAKE_ARGS"
+# Write-Host "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+
+vcpkg install --triplet x64-windows gsl boost-headers
+vcpkg integrate install
+
+New-Item -Path "Env:\CMAKE_ARGS" -Value "-DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+# Write-Host "VCPKG_ROOT=$env:VCPKG_ROOT"
+Write-Host "CMAKE_ARGS=$env:CMAKE_ARGS"
+# Write-Host "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
